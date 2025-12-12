08993d8a3e7b59adc468037a992e025cfc711d96
testgit101-local <umstinbabilan@gmail.com>
2025-12-12 00:47:11 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("helloBtn").addEventListener("click", function () {
+    document.getElementById("message").textContent =
+        "You clicked the button! JavaScript is working!";
+});
